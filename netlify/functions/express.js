38a9faf46dc432c6c6a9bd585c479bbaecc7fc1e
Savva Siry <savvasiry@gmail.com>
2024-06-06 16:14:21 +0300

    test cookies

--- netlify/functions/express.js
+++ netlify/functions/express.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 
 app.get('/users', (req, res) => {
-    res.setHeader('Set-Cookie', ['myCookie=hello; Path=/; SameSite=None; Secure; domain=yamatotest.netlify.app']);
+    res.appendHeader('Set-Cookie', ['myCookie=hello; Path=/; SameSite=None; Secure; domain=yamatotest.netlify.app']);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.send('respond with a resource');
 });
